9ae45f251c2f1a7ce5d75dab5a75f0289480fdd8
Seaw24 <ducnam883@gmail.com>
2024-11-06 23:01:03 -0800

    set db

--- YouthSavingsBE/src/App.ts
+++ YouthSavingsBE/src/App.ts
@@ -1,3 +1,21 @@
 import express from 'express';
+import { connectDB } from './db/connect';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI as string);
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
