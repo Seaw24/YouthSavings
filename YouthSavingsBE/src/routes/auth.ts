f86724853369b83dc2708a8f3bbc5a314391eb0a
Seaw24 <ducnam883@gmail.com>
2024-11-25 22:13:45 -0800

    separate the magic link route1

--- YouthSavingsBE/src/routes/auth.ts
+++ YouthSavingsBE/src/routes/auth.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { magicLink, login } from "../controllers/auth";
+import login from "../controllers/auth";
 
 const routerAuth = express.Router();
 
-routerAuth.route("/login").post(login).post(magicLink);
+routerAuth.route("/login").post(login);
 
 /* routerAuth.route("/dashboar").post(dashboar);
  */
